4caa3b7106d43917a7038d355d0b08135ed58854
Durrez Ahmed <durrezahmed3@gmail.com>
2022-03-23 19:40:03 +0530

    making a param middleware

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -3,6 +3,9 @@ const tourController = require('./../controllers/tourController');
 
 const router = express.Router();
 
+// param middleware for checking the ID
+router.param('id', tourController.checkID);
+
 router
   .route('/')
   .get(tourController.getAllTours)
