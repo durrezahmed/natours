12eaf8d7148e92083df0547afd343cd124e410c8
Durrez Ahmed <durrezahmed3@gmail.com>
2022-03-24 20:30:56 +0530

    connect our database with the express app

--- server.js
+++ server.js
@@ -1,9 +1,24 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const app = require('./app');
 
 dotenv.config({ path: './config.env' });
 
-// the app file should be always after the env variable are read
-const app = require('./app');
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log('DB connection successful!');
+  });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
