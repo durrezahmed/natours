5e52d18a6fd4968735e652953ec0e410f59fb85e
Durrez Ahmed <durrezahmed3@gmail.com>
2022-03-26 15:07:55 +0530

    handling unhandled routes

--- app.js
+++ app.js
@@ -20,11 +20,6 @@ app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-  console.log('Hello from the middleware 👋');
-  next();
-});
-
-app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
@@ -32,5 +27,14 @@ app.use((req, res, next) => {
 // mounting the routers
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+// middleware for all bad requests
+// this middleware will run after the whole code has been executed
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server!`,
+  });
+});
 
 module.exports = app;
