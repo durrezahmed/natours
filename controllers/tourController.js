056731244f91139573b103fec8d5a2ade5544cdc
Durrez Ahmed <durrezahmed3@gmail.com>
2022-03-26 22:56:21 +0530

    errors during development vs production

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -76,11 +76,7 @@ exports.updateTour = catchAsync(async (req, res, next) => {
 });
 
 exports.deleteTour = catchAsync(async (req, res, next) => {
-  const tour = await Tour.findByIdAndDelete(req.params.id);
-
-  if (!tour) {
-    return next(new AppError('No tour found with that ID', 404));
-  }
+  await Tour.findByIdAndDelete(req.params.id);
 
   res.status(204).json({
     status: 'success',
