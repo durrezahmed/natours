c8a9e0433e60d97a4e5fa3d1af5d6446c8e9bd84
Durrez Ahmed <durrezahmed3@gmail.com>
2022-03-29 01:50:23 +0530

    nested routes with express

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -2,7 +2,11 @@ const express = require('express');
 const reviewController = require('./../controllers/reviewController');
 const authController = require('./../controllers/authController');
 
-const router = express.Router();
+// mergeParams - for tourId access
+const router = express.Router({ mergeParams: true });
+
+// POST /tour/123cdf5/reviews
+// POST /reviews
 
 router
   .route('/')
