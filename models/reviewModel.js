5989b83ff8a2bef8fb9634e128b0170ad12386f4
Durrez Ahmed <durrezahmed3@gmail.com>
2022-03-29 19:04:29 +0530

    preventing duplicate reviews

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -33,6 +33,8 @@ const reviewSchema = new mongoose.Schema(
   }
 );
 
+reviewSchema.index({ tour: 1, user: 1 }, { unique: true });
+
 // populating the tour, user fields
 reviewSchema.pre(/^find/, function (next) {
   // this.populate({
